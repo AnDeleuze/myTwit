c507191aaeb28cc4cc2e2aa68bb09a9255776d57
andeleuze <katsuya.ando496@gmail.com>
2019-03-07 21:42:42 +0900

    投稿の表示

--- myTwit/app/Http/Controllers/UserController.php
+++ myTwit/app/Http/Controllers/UserController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Tweet;
+use App\Model\Tweet;
 
 class UserController extends Controller
 {
     public function index() {
         // 自分の投稿
-        $posts = Tweet::where('user_id', 1)->get();
+        $tweets = Tweet::where('user_id', 1)->get();
         // フォロー中のユーザー
         // フォローされているユーザー
-        return view('user.index');
+        return view('user.index')->with(compact('tweets'));
     }
 }
--- myTwit/resources/views/user/index.blade.php
+++ myTwit/resources/views/user/index.blade.php
@@ -1,2 +1,6 @@
-
-user page
+<h2>投稿</h2>
+<ul>
+    @foreach ($tweets as $tweet)
+        <li>{{ $tweet->content }} / {{ $tweet->created_at }}</li>
+    @endforeach
+</ul>
