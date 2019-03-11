539d85781f6dd63a0dcc7a398e32f2c49f19855a
andeleuze <katsuya.ando496@gmail.com>
2019-03-11 17:28:24 +0900

    ユーザーページに投稿フォーム追加

--- myTwit/resources/views/user/index.blade.php
+++ myTwit/resources/views/user/index.blade.php
@@ -1,3 +1,12 @@
+<h2>投稿フォーム</h2>
+<form action="/tweet" method="post">
+  content:
+  <input type="text" name="content">
+  {{csrf_field()}}
+  <input type="submit" value="submit">
+</form>
+
+
 <h2>投稿</h2>
 <ul>
     @foreach ($tweets as $tweet)
